59efeef7463c74b5f81090147eb01b9c6d9141a5
Subhamverma2407 <subhamverma2407@gmail.com>
2026-03-08 00:22:16 +0530

    push

--- check.js
+++ check.js
@@ -1,11 +1,19 @@
-const { chromium } = require("playwright");
-const cron = require("node-cron");
+const axios = require("axios");
 
 const URL =
-  "https://ticketnew.com/movies/dhurandhar-the-revenge-movie-detail-211577?frmtid=v833gyzof7"; // change to your site
+  "https://ticketnew.com/movies/dhurandhar-the-revenge-movie-detail-211577?frmtid=v833gyzof7";
 
 const BOT_TOKEN = "8710062239:AAH_s5ma268wUqaYMal85uxITQsFw9BotZI";
 const CHAT_ID = "1407253191";
+
+async function sendNtfy(message) {
+  await fetch("https://ntfy.sh/movie-slot-alert", {
+    method: "POST",
+    body: message,
+  });
+
+  console.log("ntfy notification sent");
+}
 
 async function sendTelegramNotification(message) {
   const telegramURL = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
@@ -25,36 +33,28 @@ async function sendTelegramNotification(message) {
 }
 
 async function checkWebsite() {
-  console.log("Checking website at:", new Date().toISOString());
-
-  const browser = await chromium.launch({ headless: true });
-  const page = await browser.newPage();
+  console.log("Checking:", new Date().toISOString());
 
   try {
-    await page.goto(URL, { waitUntil: "domcontentloaded" });
+    const response = await axios.get(URL);
 
-    const count = await page.locator('[aria-label="Wednesday18"]').count();
+    const html = response.data;
 
-    if (count > 0) {
+    if (html.includes('aria-label="Wednesday18"')) {
       console.log("Element found!");
+      const message = `🚨 Slot detected for 18 March!\n\n${URL}`;
 
-      await sendTelegramNotification(
-        `🚨 Element Found!\n\naria-label=" 18 March" detected on:\n${URL}`,
-      );
+      await Promise.all([sendNtfy(message)]);
+
+      //   await sendTelegramNotification(
+      //     `🚨 Slot detected for 18 March!\n\n${URL}`,
+      //   );
     } else {
       console.log("Element not found");
     }
   } catch (err) {
     console.error("Error:", err);
   }
-
-  await browser.close();
 }
 
-// run every 5 minutes
-cron.schedule("*/30 * * * *", () => {
-  checkWebsite();
-});
-
-// run once immediately
 checkWebsite();
