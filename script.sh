e69ca51d72806253d104e69437c8de5042083434
Subhamverma2407 <subhamverma2407@gmail.com>
2026-03-09 13:12:17 +0530

    code update

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /Users/subham/My\ Drive/MovieBot/movie/
-/Users/subham/.nvm/versions/node/v18.12.0/bin/node /Users/subham/My\ Drive/MovieBot/movie/check.js
+/Users/subham/.nvm/versions/node/v18.12.0/bin/node --no-warnings /Users/subham/My\ Drive/MovieBot/movie/check.js
