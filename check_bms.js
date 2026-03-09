ca51954d828b675a308b0d2923bbb9b419ba5325
Subhamverma2407 <subhamverma2407@gmail.com>
2026-03-09 13:41:50 +0530

    commit

---
+++ check_bms.js
@@ -0,0 +1,45 @@
+const URL =
+  "https://www.district.in/movies/dhurandhar-the-revenge-movie-tickets-in-bengaluru-MV211577?frmtid=v833gyzof7";
+
+async function sendNtfy(message) {
+  await fetch("https://ntfy.sh/movie-slot-alert", {
+    method: "POST",
+    body: "DISTRICT:" + message,
+  });
+
+  console.log("ntfy notification sent");
+}
+
+async function checkWebsite() {
+  console.log("Checking:", new Date().toISOString());
+
+  try {
+    const response = await fetch(URL);
+
+    const html = await response.text();
+
+    const hasDate = html.includes('aria-label="Wednesday18"');
+
+    const preferredTheatres = [
+      "Cinepolis Nexus Shantiniketan",
+      "PVR Vega City",
+      "PVR VR",
+    ];
+
+    const hasPreferredTheatre = preferredTheatres.some((t) => html.includes(t));
+
+    if (hasDate && hasPreferredTheatre) {
+      console.log("Element found!");
+
+      const message = `🚨 Slot detected for 19 March`;
+
+      await sendNtfy(message);
+    } else {
+      console.log("Element not found");
+    }
+  } catch (err) {
+    console.error("Error:", err);
+  }
+}
+
+checkWebsite();
--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd /Users/subham/My\ Drive/MovieBot/movie/
 /Users/subham/.nvm/versions/node/v18.12.0/bin/node --no-warnings /Users/subham/My\ Drive/MovieBot/movie/check.js
+/Users/subham/.nvm/versions/node/v18.12.0/bin/node --no-warnings /Users/subham/My\ Drive/MovieBot/movie/check_bms.js
