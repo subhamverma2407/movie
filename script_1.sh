82f588696ad49f7e1c526bd4d9b2c5afe642aa1f
Subhamverma2407 <subhamverma2407@gmail.com>
2026-03-10 14:27:26 +0530

    Apply updated gitignore

--- script_1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/movieBot/movie
-node_path="~/.nvm/versions/node/v25.8.0/bin/node"
-NODE_OPTIONS="--dns-result-order=ipv4first" $node_path --no-warnings check.js &
-NODE_OPTIONS="--dns-result-order=ipv4first" $node_path --no-warnings check_bms.js &
-NODE_OPTIONS="--dns-result-order=ipv4first" $node_path --no-warnings check_cinepolis_imax.js &
-wait
